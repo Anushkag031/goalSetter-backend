61465346ac99ad0bb3c7df5990cee0fab14d75c2
Anushka Gupta <anushkagupta319@gmail.com>
2024-06-03 17:50:10 +0530

    chore: Update goalRoutes to handle POST, PUT, and DELETE requests

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ message:'Get goals' }); //will show in postman
+    res.status(200).json({ message:'Get goals' }); //will show in postman
 });
+
+router.post('/', (req, res) => { //post request
+    res.status(200).json({ message:'Post goals' }); //will show in postman
+});
+
+router.put('/:id', (req, res) => { //put request (update goal)
+    res.status(200).json({ message:`Update goal ${req.params.id}` }); // passing the id
+});
+
+router.delete('/:id', (req, res) => { //delete request
+    res.status(200).json({ message:`Delete goal ${req.params.id}` }); //will show in postman
+})
 
 module.exports = router
