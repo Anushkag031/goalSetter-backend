edd22e80da9a95e0ddd9c5ebe662e52ec6207661
Anushka Gupta <anushkagupta319@gmail.com>
2024-06-03 17:38:59 +0530

    chore: Add API endpoint for retrieving goals

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+//entry point for the server
+
+const express= require('express');
+const dotenv= require('dotenv').config();
+const port = process.env.PORT ||  5001
+
+//initialize express
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+    res.json({ message:'Get goals' }); //will show in postman
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
