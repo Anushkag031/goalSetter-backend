75a9ba714bca87d46e9d811af86bc42cc97bf753
Anushka Gupta <anushkagupta319@gmail.com>
2024-06-07 14:44:46 +0530

    chore: Refactor goalController.js to use asyncHandler for better error handling

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,35 @@
+const asyncHandler = require("express-async-handler");
 //@desc get goals from db
 //@ route Get /api/goals
 //@access Private
-const getGoals = (req, res) => {
-  res.status(200).json({ message: "Get goals" }); //will show in postman
-};
+const getGoals = asyncHandler(async(req, res) => {
+    res.status(200).json({ message: "Get goals" }); //will show in postman
+  }) ;
 
 //@desc set goals from db
 //@ route POST /api/goals
 //@access Private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler( async(req, res) => {
   if (!req.body.text) {
     res.status(400) //will show in postman
     throw new Error('Please provide a text message in the request');
   }
   res.status(200).json({ message: "Set goals" }); //will show in postman
-};
+});
 
 //@desc Update goals from db
 //@ route PUT /api/goals
 //@access Private
-const updateGoals = (req, res) => {
+const updateGoals =asyncHandler( async(req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` }); // passing the id
-};
+});
 
 //@desc delete goals from db
 //@ route delete /api/goals
 //@access Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async(req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` }); //will show in postman
-};
+});
 
 module.exports = {
   getGoals,
