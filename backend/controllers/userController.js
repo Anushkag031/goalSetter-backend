b78410300283111fdab4d0acd8cb12ccbd70357d
Anushka Gupta <anushkagupta319@gmail.com>
2024-06-16 22:00:58 +0530

    refactor: Use asyncHandler for userController functions and update dependencies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,28 @@
+const jwt=require('jsonwebtoken')
+const bycrypt=require('bcryptjs')
+const asyncHandler = require("express-async-handler");
+const User = require('../models/userModel')
+
 //@desc register user
 //@ route Get /api/users
 //@access public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler( async(req, res) => {
   res.json({ message: "User registered" });
-};
+});
 
 //@desc authentication of user
 //@ route POST /api/users/login
 //@access public
-const loginUser = (req, res) => {
+const loginUser =asyncHandler( async(req, res) => {
   res.json({ message: "User login" });
-};
+});
 
 //@desc get the user profile
 //@ route Get /api/users/me
 //@access public
-const getMe = (req, res) => {
+const getMe =asyncHandler( async(req, res) => {
   res.json({ message: "User profile" });
-};
+});
 
 module.exports = {
   registerUser,
